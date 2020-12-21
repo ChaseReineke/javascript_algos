aeb09b274abf5d9493ada0368d6f52d5ecffa408
ChaseReineke <71865509+ChaseReineke@users.noreply.github.com>
2020-12-20 21:39:54 -0800

    Add files via upload

---
+++ loops_algo.js
@@ -0,0 +1,12 @@
+for (var i = 0; i < 20; i++) {
+    if (i % 2 == 1) {
+        console.log(i);
+    }
+}
+
+sum = 0;
+for (var num = 0; num < 6; num++) {
+    console.log(num);
+    sum = sum + num;
+    console.log(sum);
+}
